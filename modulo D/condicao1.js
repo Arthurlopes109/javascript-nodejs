a98e5b41b8a64ea8830a2464f28f8a23f897a45a
arhur lopes <arthurlopespereirasilva000@gmail.com>
2025-07-25 09:50:36 -0300

    comentario

--- modulo D/condicao1.js
+++ modulo D/condicao1.js
@@ -1,6 +1,6 @@
 var vel = 30
 console.log(`A velocidade do seu carro é de ${vel} Km/h`)
-if(vel > 50) {
+if(vel > 50) { // Condição simples
     console.log(`Seu carro ultrapassou a velocidade permitida, multado!!!`)
 }
 console.log('Dirija com cuidado')
