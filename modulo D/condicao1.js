f25c0acabbb4cf08728127415fa9a13af75b2b68
arhur lopes <arthurlopespereirasilva000@gmail.com>
2025-07-25 09:49:17 -0300

    ex1 de condições

---
+++ modulo D/condicao1.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if(vel > 50) {
+    console.log(`Seu carro ultrapassou a velocidade permitida, multado!!!`)
+}
+console.log('Dirija com cuidado')
