e59eabc6abcaebcefc4a92616b1334119596fe49
arhur lopes <arthurlopespereirasilva000@gmail.com>
2025-07-26 09:49:31 -0300

    condições aninhadadas

---
+++ modulo D/condicao3.js
@@ -0,0 +1,11 @@
+var idade = 10
+console.log(`Você tem ${idade} anos`)
+if (idade < 16 ) {
+    console.log('Não vota')
+} else if (idade < 18) {
+    console.log('Voto opcional')                       
+} else if(idade > 67) {
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatorio')
+}
