f585d9240c13f304ee2bcff197e7cbcb78937b67
arhur lopes <arthurlopespereirasilva000@gmail.com>
2025-07-26 10:32:13 -0300

    ex de pegar a hora

---
+++ modulo D/condicao4.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são ${hora} horas`)
+if ( hora >= 6 && hora <= 12) {
+    console.log('Bom dia')
+} else if (hora >= 0 && hora < 6) {
+    console.log('Está de madrugada vá dormir')
+} else if (hora <= 17) {
+    console.log('Boa tarde')
+} 
+ else if (hora <= 23) {
+    console.log('Boa noite')
+}
