986b6cb92f795ff7732a34f1c87e5f385d5c8f59
arhur lopes <arthurlopespereirasilva000@gmail.com>
2025-08-20 09:31:03 -0300

    salve

---
+++ modulo E/aula13/repeticao.js
@@ -0,0 +1,14 @@
+// console.log('Bom')
+// console.log('Bom')
+// console.log('Bom')
+// console.log('Bom')
+// console.log('Bom')
+// console.log('Bom')
+// console.log('Bom')
+// console.log('Bom')
+
+var contador = 1
+while (contador < 6) {
+    console.log('Bom')
+    c++
+}
