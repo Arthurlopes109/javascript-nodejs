a4a43c56701ce2ddb3706091423916d04e5d52be
arhur lopes <arthurlopespereirasilva000@gmail.com>
2025-08-29 10:18:13 -0300

    finalizado o site contagem

--- exercicios em js/contagem de passos/script.js
+++ exercicios em js/contagem de passos/script.js
@@ -1,12 +1,34 @@
 function contar() {
-    const inicio = document.getElementById('inicio')
-    const fim = document.getElementById('fim')
-    const salto = document.getElementById('salto')
-    const res = document.getElementById('resp')
+    let inicio = document.getElementById('caixa1')
+    let fim = document.getElementById('caixa2')
+    let salto = document.getElementById('caixa3')
+    let resultado = document.getElementById('resultado')
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || salto.value.length == 0){
-        alert('[erro]')
-    } else {
-        alert('ok')
+    resultado.innerHTML = "Contando: <br>"
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || salto.value.length == 0){
+        window.alert('[ERRO]')
+        resultado.innerHTML = 'Impossivel contar'
     }
+
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let s = Number(salto.value)
+
+    if(s <= 0){
+        window.alert('Passo invalido! Considerando Salto 1')
+        s = 1
+    }
+
+    if(i < f) {
+        for(let cont = i; cont <= f; cont += s) { // comtagem crescente
+        resultado.innerHTML += `${cont} \u{1F449} `
+      }
+    } else if (i > f) {
+        for(let cont2 = i; cont2 >= f; cont2-=s){ // contagem decrescente
+            resultado.innerHTML += `${cont2} \u{1F449} `
+        }
+    }
+
+    resultado.innerHTML += "\u{1F3C1}FIM"
 }
