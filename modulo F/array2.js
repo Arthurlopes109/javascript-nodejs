81e5277fbd81a35136ed659a09076ff0aea5986b
arhur lopes <arthurlopespereirasilva000@gmail.com>
2025-09-16 08:52:48 -0300

    array2

---
+++ modulo F/array2.js
@@ -0,0 +1,6 @@
+// Percuso para exibição de um vertor
+let num = [3,5,7,2,9]
+console.log(num) 
+for(let pos = 0; pos<num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
