3498982f31a22cfee0bddf3d88e3dd8b25cd7805
Arthur Lopes <arthurlopespereirasilva000@gmail.com>
2025-10-07 09:44:56 -0300

    analizador de numeros completo

--- exercicios em js/analizador de numeros/script.js
+++ exercicios em js/analizador de numeros/script.js
@@ -21,8 +21,42 @@ function inLista(n, l){
 
 function start(){
    if(isNumero(numeros.value) && !inLista(numeros.value, valores)){
-        window.alert("tudo ok")
-   }else {
+       valores.push(Number(numeros.value))
+       let item = document.createElement('option')
+       item.text = `Valor ${numeros.value} adicionado.`
+       lista.appendChild(item)
+       res.innerHTML = ''
+    }else {
         window.alert('Valor invalido ou já está na lista.')
    }
+   numeros.value = ''
+   numeros.focus()
+}
+
+function end() {
+    if(valores.length == 0){
+        window.alert('O programa não pode iniciar pois nenhum valor foi adicionado!')
+    }else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let posi in valores) {
+            soma += valores[posi]
+            if(valores[posi] > maior) {
+                maior = valores[posi]
+            }
+            if(valores[posi] < menor) {
+                menor = valores[posi]
+            }
+        }
+        media = soma / total
+        res.innerHTML =''
+        res.innerHTML+= `<p>Ao todo, temos <strong>${total}</strong> números cadastrados</p>`
+        res.innerHTML+= `<p>O maior valor adicionado é <strong>${maior}</strong></p>`
+        res.innerHTML+= `<p>O menor valor adicionado é <strong>${menor}</strong></p>`
+        res.innerHTML+= `<p>A soma de todos o valores é <strong>${soma}</strong></p>`
+        res.innerHTML+= `<p>A media de todos o valores é <strong>${media}</strong></p>`
+    }
 }
